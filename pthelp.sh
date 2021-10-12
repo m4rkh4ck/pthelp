145ecec58decafef9b219e0e76489135de190aa0
m4rkh4ck <92309458+m4rkh4ck@users.noreply.github.com>
2021-10-12 10:06:05 +0200

    Update pthelp.sh

--- pthelp.sh
+++ pthelp.sh
@@ -6,6 +6,7 @@ clear
 #Color variables
 RED='\033[1;31m'
 GREEN='\033[0;32m'
+GREEN1='\033[1;32m'
 WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
 LCYAN="\e[1;36m"
@@ -38,7 +39,7 @@ read url
 
 echo -e "${WHITE}"
 PS3='Choose what to do ---> '
-options=("SPAWN NEW TERMINAL" "OPENVPN" "KILLVPN" "NET INTERFACES" "OPEN IN BROWSER" "HARVESTING" "HASH IDENTIFIER" "SHELL TTY" "REVERSE_SHELL_PHP" "REVERSE SHELL CHEAT SHEETS" "INTENSE SCAN" "FAST SCAN" "CUSTOM SCAN" "WORDPRESS SCAN" "DIRB" "OPEN METASPLOIT" "OPEN WIRESHARK" "SEARCH A PUBLIC EXPLOIT" "SSH" "FTP" "Quit")
+options=("SPAWN NEW TERMINAL" "OPENVPN" "KILLVPN" "NET INTERFACES" "OPEN IN BROWSER" "HARVESTING" "HASH IDENTIFIER" "SHELL TTY" "REVERSE_SHELL_PHP" "REVERSE SHELL CHEAT SHEETS" "INTENSE SCAN" "FAST SCAN" "CUSTOM SCAN" "WORDPRESS SCAN" "DIRB" "OPEN METASPLOIT" "OPEN WIRESHARK" "SEARCH A PUBLIC EXPLOIT" "GTFOBins" "SSH" "FTP" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -201,6 +202,17 @@ do
              searchsploit $cve
              echo -e "${WHITE}"
              ;;
+        "GTFOBins")
+             echo -e "${GREEN1}"
+             echo "GTFOBins is a curated list of Unix binaries that can be used to bypass local security restrictions"
+             echo "in misconfigured systems"
+             echo -e "${YELLOW}"
+             echo "Type your low privilege user"
+             read utente1
+             runuser -u $utente1 -- firefox https://gtfobins.github.io/ &
+             echo -e "${WHITE}"
+             ;;
+
         "SSH")
              echo -e "${YELLOW}"
              echo "Please digit ssh-user"
